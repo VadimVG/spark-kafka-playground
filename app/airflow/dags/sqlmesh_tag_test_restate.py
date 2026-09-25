e6c55b3f2ab08423a02992161d12e9f1c8d543df
Vadim <115628007+VadimVG@users.noreply.github.com>
2026-09-25 10:44:37 +0300

    created sqlmesh-airflow dags

---
+++ app/airflow/dags/sqlmesh_tag_test_restate.py
@@ -0,0 +1,45 @@
+import os
+from datetime import datetime
+
+from airflow.sdk import DAG
+from airflow.providers.docker.operators.docker import DockerOperator
+from docker.types import Mount
+
+with DAG(
+    "sqlmesh_tag_test_restate",
+    schedule="0 2 * * *",
+    start_date=datetime(2026, 1, 1),
+    catchup=False,
+    max_active_runs=1,
+):
+    DockerOperator(
+        task_id="sqlmesh_tag_test_restate",
+        # pool="sqlmesh_test",
+        image="sqlmesh:local",
+        command=(
+            'sqlmesh plan --restate-model "tag:test" '
+            "--no-prompts --auto-apply"
+        ),
+        docker_url="unix://var/run/docker.sock",
+        network_mode="data-platform-net",
+        auto_remove="success",
+        mount_tmp_dir=False,
+        user=os.environ.get("UID_GID"),
+        mounts=[
+            Mount(
+                source=f"{os.environ['HOST_PROJECT_DIR']}/app/sqlmesh",
+                target="/opt/app/sqlmesh",
+                type="bind",
+            )
+        ],
+        environment={
+            k: os.environ[k]
+            for k in (
+                "POSTGRES_HOST",
+                "POSTGRES_PORT",
+                "POSTGRES_USER",
+                "POSTGRES_PASSWORD",
+                "POSTGRES_DB",
+            )
+        },
+    )
